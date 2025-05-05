b183c41be74f9463f338f5c83e6aa83745b440b3
2paperstar <lee@paperst.ar>
2025-05-06 02:44:22 +0900

    fix: idp dio base url

--- lib/modules/core/data/dio/idp_dio.dart
+++ lib/modules/core/data/dio/idp_dio.dart
@@ -2,4 +2,8 @@ import 'package:dio/browser.dart';
 import 'package:injectable/injectable.dart';
 
 @singleton
-class IdPDio extends DioForBrowser {}
+class IdPDio extends DioForBrowser {
+  IdPDio() {
+    options.baseUrl = 'https://api.idp.gistory.me/';
+  }
+}
