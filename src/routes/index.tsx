64abb03c6a37f27d87ea5d2f498bc1e7870340fb
Juwon Yoo <juwoyoo@gmail.com>
2026-01-29 17:29:05 +0900

    feat: 메인 페이지 UI 개편 및 명함 컴포넌트 디자인 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,7 @@ import { useState, useEffect } from "react";
 import BusinessCard from "../components/BusinessCard";
 import skating_icon_Default from "../assets/icons/skating_icon_Default.svg";
 import snowflake_1 from "../assets/icons/snowflake_1.svg";
+import snowflake_2 from "../assets/icons/snowflake_2.svg";
 import snowflake_4 from "../assets/icons/snowflake_4.svg";
 
 export const Route = createFileRoute("/")({
@@ -46,35 +47,42 @@ function MainPage() {
   const realCards = [
     {
       name: "지니",
-      id: "20230001",
+      studentId: "20230001",
       email: "example@gist.ac.kr",
-      color: "purple",
-      issueDate: "2026.01.02",
+      centerColor: "blue",
+      instagramId: "@aaa",
+      department: "전기전자컴퓨터공학과",
     },
     {
       name: "어스",
-      id: "20240001",
+      studentId: "20240001",
       email: "example@gist.ac.kr",
-      color: "orange",
-      issueDate: "2026.01.03",
+      centerColor: "purple",
+      instagramId: "@bbb",
+      department: "전기전자컴퓨터공학과",
+    },
+    {
+      name: "예시",
+      studentId: "20250001",
+      email: "example@gist.ac.kr",
+      centerColor: "pink",
+      instagramId: "@ccc",
+      department: "도전탐색과정",
     },
   ];
 
   return (
-    <div className="w-full h-full overflow-y-auto scrollbar-hide overflow-x-hidden">
-      <div className="relative min-h-full w-full bg-blue-200 bg-[linear-gradient(162deg,#D2E4FF,#E9E0FF)] flex flex-col justify-start items-center">
-        {/* 테스트용 로그아웃 버튼 */}
-        <div className="absolute bottom-[40px] z-50">
-          <button className="text-gray-400 text-xs" onClick={handleLogoutClick}>
-            테스트용 로그아웃 (토큰/프로필 초기화)
-          </button>
-        </div>
-
+    <div className="w-full h-[100dvh] overflow-y-auto scrollbar-hide overflow-x-hidden">
+      <div className="relative min-h-[100dvh] w-full bg-blue-200 bg-[linear-gradient(162deg,#D2E4FF,#E9E0FF)] flex flex-col justify-start items-center bg-fixed">
         {/* 배경 장식*/}
         <div>
           <img
             className="absolute left-1/2 -translate-x-1/2 ml-[95px] top-[29px] w-[35px] h-[35px]"
             src={snowflake_1}
+          />
+          <img
+            className="absolute left-1/2 -translate-x-1/2 ml-[-139px] bottom-[88px] w-[57px] h-[57px]"
+            src={snowflake_2}
           />
           <img
             className="absolute left-1/2 -translate-x-1/2 ml-[134px] top-[159px] w-[65px] h-[65px]"
@@ -105,7 +113,7 @@ function MainPage() {
         </div>
 
         <div
-          className={`z-10 w-full flex flex-col justify-start items-center gap-[16px] pt-[20px] pb-[20px] mb-[35px] ${
+          className={`z-10 w-full flex flex-col justify-start items-center gap-[16px] pt-[20px] pb-[20px] mb-[24px] ${
             !isProfileRegistered ? "blur-[5px]" : ""
           }`}
         >
@@ -116,37 +124,48 @@ function MainPage() {
                   name="지니"
                   studentId="20260001"
                   email="example@gm.gist.ac.kr"
-                  centerColor="newBlue"
-                  issueDate="2026.01.01"
+                  centerColor="blue"
+                  instagramId="@aaa"
+                  department="도전탐색과정"
                 />
               ))
             : realCards.map((card, index) => (
                 <BusinessCard
                   key={index}
                   name={card.name}
-                  studentId={card.id}
+                  studentId={card.studentId}
                   email={card.email}
-                  centerColor={card.color}
-                  issueDate={card.issueDate}
+                  centerColor={card.centerColor}
+                  instagramId={card.instagramId}
+                  department={card.department}
                 />
               ))}
         </div>
 
-        {!isProfileRegistered ? (
-          <Link
-            to="/profile"
-            className="flex shrink-0 justify-center items-center w-[280px] h-[50px] rounded-[100px] mb-[71px] bg-[#9EB6FF] text-[20px] text-white font-bold"
+        <div className="w-full mt-auto pb-[calc(30px+env(safe-area-inset-bottom))] flex flex-col items-center gap-[10px] z-50">
+          {!isProfileRegistered ? (
+            <Link
+              to="/profile"
+              className="flex shrink-0 justify-center items-center w-[280px] h-[50px] rounded-[100px] bg-[#9EB6FF] text-[20px] text-white font-bold shadow-lg"
+            >
+              프로필 등록하기
+            </Link>
+          ) : (
+            <Link
+              // 채팅방 만들어지면 연결
+              className="flex shrink-0 justify-center items-center w-[280px] h-[50px] rounded-[100px] bg-[#9EB6FF] text-[20px] text-white font-bold shadow-lg"
+            >
+              채팅방 참여하기
+            </Link>
+          )}
+
+          <button
+            className="text-gray-400 text-xs mb-2"
+            onClick={handleLogoutClick}
           >
-            프로필 등록하기
-          </Link>
-        ) : (
-          <Link
-            // 채팅방 만들어지면 연결
-            className="flex shrink-0 justify-center items-center w-[280px] h-[50px] rounded-[100px] mb-[71px] bg-[#9EB6FF] text-[20px] text-white font-bold"
-          >
-            채팅방 참여하기
-          </Link>
-        )}
+            테스트용 로그아웃 (토큰/프로필 초기화)
+          </button>
+        </div>
       </div>
     </div>
   );
